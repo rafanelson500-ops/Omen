28799242bb06d00a21b35d487ef69e0dd10c0f15
ZachNewc <newcomer.zach@gmail.com>
2026-03-03 13:09:38 -0600

    Ai automation

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, emit
 from api.main import start_api
+from engine.main import start_engine
 
 load_dotenv()
 
@@ -12,5 +13,7 @@ socketio.init_app(app, cors_allowed_origins="*")
 
 start_api(socketio)
 
-if __name__ == "__main__":
-    socketio.run(app, debug=True, port=8000, host='0.0.0.0')
+start_engine()
+
+# if __name__ == "__main__":
+#     socketio.run(app, debug=True, port=8000, host='0.0.0.0')
